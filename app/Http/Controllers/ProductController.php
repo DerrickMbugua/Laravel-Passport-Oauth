1fc405e95f4c33ba75a6d5f2bce2bcb0549d3ba8
Derrick Mwema <derrickmbugua50@gmail.com>
2021-05-31 09:57:58 +0300

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $products = $request->validate([
+            'name' => 'string|required',
+            'description' => 'string|required',
+            'slug' => 'string|required',
+            'price' => 'integer|required'
+        ]);
+        return Product::create($products);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return Product::find($product->id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+
+        $products=Product::find($product->id);
+        $products->update($request->all());
+        return $products;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        return Product::destroy($product->id);
+    }
+}
